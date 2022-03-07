2a40fa1a0bb1eb0436aeda2c09814cb48c1b0065
Ethan Mann <emannguitar@gmail.com>
2022-03-07 18:13:29 -0500

    Setting up workspace, Inital migration of code

---
+++ cli/src/lib.rs
@@ -0,0 +1,10 @@
+#![warn(clippy::all)]
+#![warn(clippy::correctness)]
+#![warn(clippy::style)]
+#![warn(clippy::complexity)]
+#![warn(clippy::perf)]
+
+use std::fmt::Debug;
+
+pub mod prompt;
+pub mod from_cli;
